599051b5b0bacc6912dce76fb5ebe5d30240f371
Gustavo <unidragorn@gmail.com>
2022-09-12 20:24:16 -0300

    adiciona código average

--- src/average.js
+++ src/average.js
@@ -11,6 +11,23 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+
+  let sum = 0;
+
+  if (array.length < 1) {
+    return undefined;
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    sum += array[i];
+  }
+
+  sum = Math.round(sum / (array.length));
+  return sum;
+};
 
 module.exports = average;
